15134ccbdc7143bfa9b475091beddf471acbd8fa
ChDoTwo <engnl322@gmail.com>
2022-06-30 08:56:40 +0900

    End_Earth_file

--- 02_Earth/02_js/main.js
+++ 02_Earth/02_js/main.js
@@ -1,3 +1,8 @@
+/* 새로 고침 or 첫 화면 가장 맨위 + 살짝 무빙 */
+$('html').animate({scrollTop:'0'},10);
+$('.content_area').addClass('on');
+
+
 /* Header */
 $(window).on('scroll',function(){
     if($(window).scrollTop() == 0){
@@ -9,21 +14,31 @@ $(window).on('scroll',function(){
 
 
 /* Example Tab Menu */
-$('.tab .wrap').removeClass('show');
-$('.tab .wrap').eq(0).addClass('show');
-
-$('#Example .tab_menu').on('click','li',function(){
+class_opt('.tab .wrap',0);
+class_opt('.tab_list li',0);
+$('#Example .tab_list').on('click','li',function(){
     var tab_i = $(this).index();
 
-    $('.tab .wrap').removeClass('show');
-    $('.tab .wrap').eq(tab_i).addClass('show');
+    class_opt('.tab .wrap',tab_i);
+    class_opt('.tab_list li',tab_i);
 });
 
 
 /* Gallery */
+class_opt('#Gallery .image_box',0);
 $('#Gallery .image_box').on('click',function(){
+    var box_i = $(this).index();
+
     $('#Gallery .main_image').animate({'opacity':0},50,function(){   // 깜빡임 표현
         $('#Gallery .main_image').animate({'opacity':1},350)
     });
-    $('#Gallery .main_image').html($(this).html());
+    $('#Gallery .main_image').html($(this).html()); // main image 변경
+    class_opt('#Gallery .image_box',box_i); // .image_box show evnet
 });
+
+
+/* class_opt */
+function class_opt(name, i) {
+    $(name).removeClass('show');
+    $(name).eq(i).addClass('show');
+}
