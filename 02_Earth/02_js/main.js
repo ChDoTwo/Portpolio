6b4caea0c8f4feb2a6caa6f738d1306e410f3000
ChDoTwo <engnl322@gmail.com>
2022-06-29 10:51:15 +0900

    Update_Earth_JS

--- 02_Earth/02_js/main.js
+++ 02_Earth/02_js/main.js
@@ -1,4 +1,14 @@
+/* Header */
+$(window).on('scroll',function(){
+    if($(window).scrollTop() == 0){
+        $('#Header').removeClass('Top_fixed');
+    }else {
+        $('#Header').addClass('Top_fixed');
+    }
+});
 
+
+/* Example Tab Menu */
 $('.tab .wrap').removeClass('show');
 $('.tab .wrap').eq(0).addClass('show');
 
@@ -7,4 +17,13 @@ $('#Example .tab_menu').on('click','li',function(){
 
     $('.tab .wrap').removeClass('show');
     $('.tab .wrap').eq(tab_i).addClass('show');
+});
+
+
+/* Gallery */
+$('#Gallery .image_box').on('click',function(){
+    $('#Gallery .main_image').animate({'opacity':0},50,function(){   // 깜빡임 표현
+        $('#Gallery .main_image').animate({'opacity':1},350)
+    });
+    $('#Gallery .main_image').html($(this).html());
 });
