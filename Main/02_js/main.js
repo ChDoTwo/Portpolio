ec9ea915053b074d6864c7b9603fa1570b53675a
ChDoTwo <engnl322@gmail.com>
2022-07-11 13:04:16 +0900

    Update_MainPage_JS

--- Main/02_js/main.js
+++ Main/02_js/main.js
@@ -1,81 +1,115 @@
-/* 새로 고침 or 첫 화면 가장 맨위 + 살짝 무빙 */
-var win_w = $(window).innerWidth();
-var win_h = $(window).innerHeight(); // window height
-var header_h = $('#header').height();
-var home_h = $('#Home').height();
+// resize시
+$(window).on('resize',function(e){
+    reset_opt();
+    resize_e(e);
+});
+$(window).trigger('resize'); // resize event 강제 실행
 
-square_opt('#Portpolio table td') // portpolio td 정사각형 설정
-square_opt('#Skill table tr:first-child td') // skill td 정사각형 설정
-
-/* Header */
-$(window).on('scroll',function(){
-    var win_t = $(window).scrollTop(); // window scroll 위치
-
-    if(win_t <= (home_h - header_h)){
-        $('#header').removeClass('trans_h');
-    }else {
-        $('#header').addClass('trans_h');
-    }
+var isStop = false; // 스크롤 반복 재생 stop
+$(window).on('scroll',function(e){
+    scroll_e(e);
 });
 
 /* logo click */
-var call_li = false; // Call Menu 클릭 여부 확인 변수
 $('.logo').click(function(){
-    reset_opt(); // 첫 상태로 reset
-    $('html').animate({scrollTop:'0'},400); // 맨 위로 이동
+    reset_opt(); // 첫 상태로 reset ( 맨위로 이동 )
 });
 
 /* nav click */
-reset_opt();
 $('.nav').on('click','li',function(){
-    var li_i = $(this).index();
-    var toContent_t = $('#'+$(this).text()).offset().top; // 선택한 li content 상위 포인트
-    var toContent_h = $('#'+$(this).text()).height(); // 선택한 li content 높이
+    var [win_w, win_h, win_t, header_h, home_h] = size_check(); // size 체크
+    var [toContent_t, toContent_h] = size_check($(this).text()); // content 상위 포인트와 높이 체크
 
-    if(li_i == 4){ // call click
-        $('#Call').animate({height:'15rem'},400,function(){
-            $('#Call').children('table').animate({opacity:'1'},400);
-        });
+    if($('.call').hasClass('on') == false){ // Call이 click이 안 되어 있다면
+        $('.nav li').removeClass('on');
         $(this).addClass('on');
-        call_li = true;
-    }else if(call_li == true) { // Call이 click이 되어 있다면
-        class_opt('.nav li',3,'on'); // li event ( Call li는 꺼지지 않게 설정 )
-        $(this).addClass('on');
-        $('html').animate({scrollTop:((toContent_t + win_h) - (toContent_h + win_h))},500);
-    }else if(call_li == false){ // Call이 click이 안 되어 있다면
-        $('.nav li').removeClass('on'); // li event (li 다 꺼지게 설정)
-        $(this).addClass('on');
-        $('html').animate({scrollTop:((toContent_t + win_h) - (toContent_h + win_h))},500);
+
+        if($(this).hasClass('call') == true){ // Call을 click 한거라면
+            $('#Call').animate({height:'15rem'},400,function(){ // Call Menu 열기
+                $('#Call').children('table').animate({opacity:'1'},400);
+            });
+        }else{ 
+            $('html').animate({scrollTop:((toContent_t + win_h) - (toContent_h + win_h))},500); // 아니라면 해당 개체로 이동
+        }
+    }else { // Call이 click이 되어 있다면
+        $('.nav li').removeClass('on');
+        $('.call').addClass('on'); // call class on
+        $(this).addClass('on'); // 선택한 class on
+
+        $('html').animate({scrollTop:((toContent_t + win_h) - (toContent_h + win_h))},500); // 클릭한 개체로 이동
     }
 });
 
-/* skill */
-var percent = []; // percent 값 배열 선언
-var percent_color = ['#ff8a00','#003cff','#00ff66','#009cff']; // td percent color 색상 배열 선언
-var skill_td = $('#Skill table tr').eq(0).children('td'); // pie td 구역
-
-for(var i = 0; i < skill_td.length; i++){ 
-    percent.push(skill_td.eq(i).children('.percent').data('per')); // td data-per 값 추출하여 percent 배열에 push
-    draw(percent[i], i, percent_color[i]);
-}
-
-
 /* call close_btn click */
 $('#Call .close_btn').click(function(){
-    $('.nav li').eq(3).removeClass('on');
-    call_li = false;
+    $('.call').removeClass('on');
     $('#Call').animate({height:'0'},400,function(){
         $('#Call').children('table').animate({opacity:'0'},400);
     });
 });
 
+// resize 시
+function resize_e(e){
+    square_opt('#Portpolio table td') // portpolio td 정사각형 설정
+    square_opt('#Skill table tr:first-child td') // skill td 정사각형 설정
+}
+
+// scroll 시
+function scroll_e(e){
+    var [win_w, win_h, win_t, header_h, home_h] = size_check(); // size 체크
+    var [toContent_t, toContent_h] = size_check('Skill'); // content 상위 포인트와 높이 체크
+
+    if(win_w > 768){
+        if(win_t <= (home_h - header_h)){ // header의 위치가 home content 영역을 벗어나면
+            $('#header').removeClass('trans_h');
+        }else {
+            $('#header').addClass('trans_h');
+        }
+
+        skill_draw();
+    }else {
+
+    } 
+}
+
+/* size 확인 */
+function size_check(name){
+    if (name == undefined){ // 아무것도 입력하지 않았을 경우
+        var win_w = $(window).innerWidth(); // 크기 변경시 자동 width 값 계산
+        var win_h = $(window).innerHeight(); // window height
+        var win_t = $(window).scrollTop(); // window scroll 위치
+        var header_h = $('#header').height();
+        var home_h = $('#Home').height();
+
+        return [win_w, win_h, win_t, header_h, home_h];
+    }else {
+        var toContent_t = $('#'+name).offset().top; // 선택한 name 상위 포인트
+        var toContent_h = $('#'+name).height(); // 선택한 name 높이
+
+        return [toContent_t, toContent_h];
+    }
+}
+
+/* skill pie 설정 */
+function skill_draw(){
+    var percent = []; // percent 값 배열 선언
+    var percent_color = ['#ff8a00','#003cff','#00ff66','#009cff']; // td percent color 색상 결정
+    var skill_td = $('#Skill table tr').eq(0).children('td'); // pie td 구역
+
+    for(var i = 0; i < skill_td.length; i++){ 
+        percent.push(skill_td.eq(i).children('.percent').data('per')); // td data-per 값 추출하여 percent 배열에 push
+        draw(percent[i], i, percent_color[i], skill_td);
+    }
+}
+
+
 /* skill pie draw */
-function draw(max, td_n, color_n){
+function draw(max, td_n, color_n, class_n){
     var i = 1;
     var set_i = setInterval(function(){
         if(i<=max){     
-            skill_td.eq(td_n).css('background','conic-gradient('+color_n+' 0% '+i+'%, rgba(0,0,0,0.1) '+i+'% 100%)'); // 원 채우기
-            skill_td.eq(td_n).children('.percent').text(i +'%'); // percent text 값 증가
+            class_n.eq(td_n).css('background','conic-gradient('+color_n+' 0% '+i+'%, rgba(0,0,0,0.1) '+i+'% 100%)'); // 원 채우기
+            class_n.eq(td_n).children('.percent').text(i +'%'); // percent text 값 증가
             i++;
         }else {
             clearInterval(set_i);
@@ -85,21 +119,18 @@ function draw(max, td_n, color_n){
 
 /* 정사각형 설정 */
 function square_opt(name){
-    var name_w = $(name).width();
-    $(name).height(name_w);
+    var name_w = $(name).width(); // 설정한 class name의 width값 생성
+    $(name).height(name_w); // 그 설정한 class name의 height값에 width을 대입
 }
 
 /* reset */
 function reset_opt(){ // 첫 상태
-    class_opt('.nav li',0,'on'); // 첫 nav click 설정
-    call_li = false; // Call menu 클릭x
-    $('#Call').animate({height:'0'},400,function(){
+    $('.nav li').removeClass('on');
+    $('.nav li').eq(0).addClass('on'); // 첫 nav click 설정
+
+    $('#Call').animate({height:'0'},400,function(){ // Call menu 닫기
         $('#Call').children('table').animate({opacity:'0'},400);
     });
-}
 
-/* Remove Add class */
-function class_opt(name, index, class_n){
-    $(name).removeClass(class_n);
-    $(name).eq(index).addClass(class_n);
+    $('html').animate({scrollTop:'0'},400); // 맨 위로 이동
 }
